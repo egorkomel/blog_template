a91d23b011ae57cc5d90fd21f38f63f3a8a5cb0a
egorkomel <egorkomel@gmail.com>
2022-08-29 15:30:37 +0500

    created models Post, add 15 posts and make view on main page plus paggination

--- blog/myblog/models.py
+++ blog/myblog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from ckeditor_uploader.fields import RichTextUploadingField
 
-# Create your models here.
+
+class Post(models.Model): # создаем класс Post (таблицу в БД) и поля
+    h1 = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    url = models.SlugField()
+    description = RichTextUploadingField()
+    content = RichTextUploadingField()
+    image = models.ImageField()
+    created_at = models.DateField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)  # создаем отношение с табл. User
+    tag = models.CharField(max_length=200)
+
+    def __str__(self):  # нужна, чтобы в админке посты отображались по title, а не по id
+        return self.title
--- blog/myblog/views.py
+++ blog/myblog/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.views import View
+from django.core.paginator import Paginator
+from .models import Post
+
 
 class MainView(View):
     def get(self, request, *args, **kwargs):
-        return render(
-            request,
-            'myblog/home.html'
-        )
+        posts = Post.objects.all()
+        paginator = Paginator(posts, 3)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
+        return render(request, 'myblog/home.html', context={
+            'page_obj': page_obj,
+        })
