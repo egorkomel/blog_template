4f90a3b5bad021802636e9e2a60c003db78dacef
egorkomel <egorkomel@gmail.com>
2022-08-31 12:01:34 +0500

    created contact-page and success-page

--- blog/myblog/form.py
+++ blog/myblog/form.py
@@ -64,3 +64,39 @@ class SignInForm(forms.Form):
         })
     )
 
+
+class FeedBackForm(forms.Form):
+    name = forms.CharField(
+        max_length=100,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'id': 'name',
+            'placeholder': "Ваше имя",
+        })
+    )
+
+    email = forms.CharField(
+        max_length=100,
+        widget=forms.EmailInput(attrs={
+            'class': 'form-control',
+            'id': 'email',
+            'placeholder': "Ваша почта"
+        })
+    )
+
+    subject = forms.CharField(
+        max_length=200,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'id': 'subject',
+            'placeholder': "Тема",
+        })
+    )
+
+    message = forms.CharField(
+        widget=forms.Textarea(attrs={
+            'class': 'forms-control md-textarea',
+            'id': 'message',
+            'placeholder': "Ваше сообщение",
+        })
+    )
--- blog/myblog/views.py
+++ blog/myblog/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import View
 from django.core.paginator import Paginator
+from django.core.mail import send_mail, BadHeaderError
 from .models import Post
-from .form import SignUpForm, SignInForm
+from .form import SignUpForm, SignInForm, FeedBackForm
 from django.contrib.auth import login, authenticate
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 
 
 class MainView(View):
@@ -45,6 +46,7 @@ class SignUpView(View):
             'form': form,
         })
 
+
 class SignInView(View):
     def get(self, request, *args, **kwargs):
         form = SignInForm()
@@ -63,4 +65,48 @@ class SignInView(View):
                 return HttpResponseRedirect('/')
         return render(request, 'myblog/signin.html', context={
             'form': form,
+        })
+
+
+class FeedBackView(View):
+    def get(self, request, *args, **kwargs):
+        form = FeedBackForm()
+        return render(request, 'myblog/contact.html', context={
+            'form': form,
+            'title': "Написать мне",
+        })
+
+    def post(self, request, *args, **kwargs):
+        form = FeedBackForm(request.POST)
+
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            from_email = form.cleaned_data['email']
+            subject = form.cleaned_data['subject']
+            message = form.cleaned_data['message']
+
+            try:
+                # send_mail(f'От {name} на тему {subject}', message, from_email, ['komel97@yandex.ru'])
+                # отправка писем не работает, т.к. проект в локальной сети
+                # поэтому сделана просто переадресация на страницу с успешной отправкой письма
+                return render(request, 'myblog/success.html', context={
+                    'title': "Сообщение отправлено",
+                    'name': name,
+                    'from_email': from_email,
+                    'subject': subject,
+                    'message': message,
+                })
+            except BadHeaderError:
+                return HttpResponse('Невалидный заголовок')
+            return HttpResponseRedirect('Success')
+
+        return render(request, 'myblog/contact.html', context={
+            'form': form,
+        })
+
+
+class SuccessView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'myblog/success.html', context={
+            'title': "Спасибо :3",
         })
