e48eb7019969181c03413874fc44e281438aba77
egorkomel <egorkomel@gmail.com>
2022-09-05 12:32:11 +0500

    configure admin-view posts and comments

--- blog/myblog/admin.py
+++ blog/myblog/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 
 class PostAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('author', 'title', 'tag', 'created_at')
+    list_filter = ('author', 'tag', 'created_at')
+
 
 admin.site.register(Post, PostAdmin)
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('username', 'post', 'text', 'created_at')
+    list_filter = ('username', 'created_at')
+
+
+admin.site.register(Comment, CommentAdmin)
